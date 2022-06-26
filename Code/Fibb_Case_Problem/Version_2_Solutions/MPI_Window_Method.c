208480c4d95f16bc6dc65a3e3d9972771576d01a
keanu-j-a <107134356+keanu-j-a@users.noreply.github.com>
2022-06-26 10:52:11 -0600

    Update MPI_Window_Method.c

--- Code/Fibb_Case_Problem/Version_2_Solutions/MPI_Window_Method.c
+++ Code/Fibb_Case_Problem/Version_2_Solutions/MPI_Window_Method.c
@@ -3,111 +3,142 @@
 #include <stdlib.h>
 #include <tgmath.h>
 
+
 /*
+    Keanu Jonas Ammons
+    Modified on: June 23rd, 2022
 
-Keanu Jonas Ammons
-Modified on: June 23rd, 2022
-INL Parallel Algorithim Comparison
+    Example on how to send an array to another array using two different array
+    formats.
 
-This code block will solve an advanced form of the fibb sequence problem
-using one-sided communication methods.
+        Note that  int* window_buffer = (int*)malloc(2 * sizeof(int));
+        is a common choice for the window_buffers, but I currently do not know how
+        to use this with the MPI_Accumulate
 
+        we can work around this by specifying an array of variable array, assigning all
+        of its values with a for loop, and assigning additional values later.
+
+    side note: perhaps the best way to utilize MPI_Accumulate is to build an array from a list of
+    values, use accumulate function to add values between processes, and sum the final answer by use of
+    a for loop.
 */
 
-int 
-main(int argc, char* argv[]) {
-
-    // initalize al; variables for MPI and one-sided communication
+int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
     int size, rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int upperLimit = 33;
+
+    ////////////define sorting algorithim///////////////////////
+
+    const int upperLimit = 35;
 
     // define sorting algorithim
-    int start_val = rank * (upperLimit / size) + 1;
-	int num1 = 1; int num2 = 1; int SumNum = num1 + num2; int finalSum = 0; int end_val;
-	double start, end;
-    
+    const int start_val = rank * (upperLimit / size) + 1;
+    int num1 = 1; int num2 = 1; int SumNum = num1 + num2; int end_val;
+    // double start, end;
+
 
     if (rank == (size - 1)) {
-		end_val = upperLimit;
-	}
-	else {
-		end_val = start_val + (upperLimit / size) - 1;
-	}
-
-    // define arrays to be used
-    const int arraySize = end_val - start_val;
-    int window_buffer; int interSum = 0; // (int*)malloc(sizeof(int));
-
-    // generate a dynamic memory allocation in bytes
-    int* comp_array;
-    comp_array = (int*)malloc(arraySize * sizeof(int));
-
-
-    long double fibb_const = (1 + sqrt(5)) / 2;  int j = start_val - 1; long double fibb_const1 = (1 - sqrt(5)) / 2;
-	// calculate fibb sequence positions for the sequence
-	double position11 = (pow(fibb_const, j) - pow(fibb_const1, j)) / (sqrt(5));
-	double position12 = (pow(fibb_const, j + 1) - pow(fibb_const1, (j + 1))) / (sqrt(5));
-	position11 = floor(position11);
-	position12 = floor(position12);
-
-    
-    // generate a dynamic window for memeory allocation in future computations
-    if (rank == 0) {
-        for (int i = start_val; i < end_val; i++){
-
-			SumNum = num1 + num2;
-			num1 = num2;
-			num2 = SumNum;
-            if (SumNum % 2 == 0) {
-                comp_array[i] = SumNum;
-                interSum = comp_array[i] + interSum;
-                printf("%d, ", comp_array[i]);
-                fflush(stdout);
-            }
-        }
-        
+        end_val = upperLimit;
     }
-	else {
-		for (int i = start_val; i < end_val; i++) {
-
-			SumNum = position12 + position11;
-            position11 = position12;
-			position12 = SumNum;
-		    if (SumNum % 2 == 0) {
-                comp_array[i] = SumNum;
-                interSum = comp_array[i] + interSum;
-                printf("%d, ", comp_array[i]);
-                fflush(stdout);
-			}
-		}
-	}
-    MPI_Win win;
-    // make all array variables avaialble on RMA
-
-    MPI_Win_create(&window_buffer, sizeof(unsigned long), sizeof(unsigned long), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
-    MPI_Win_fence(0, win);
-
-    // group function should be used here
-    MPI_Accumulate(&interSum, 1, MPI_INT, 0, 0, 1, MPI_INT, MPI_SUM, win);
-
-    MPI_Win_fence(0, win);
-
-    // earning, this section of code is serial
-    // printf("Size = %d", arraySize);
-
-    if (rank == 0) {
-        printf("The value is %d \n", window_buffer);
+    else {
+        end_val = start_val + (upperLimit / size) - 1;
     }
-      
+
+    ////////////define sorting algorithim///////////////////////
+
+    int arraySize = end_val - start_val;
+    int window_buffer[upperLimit];
+    int* comp_array = (int*)malloc(arraySize * sizeof(int));
+    int finalSum = 0;
+ 
+    ////////////define a unqiue array on each process///////////////////////
+    //printf("This is %d 's start value: %d \n \n", rank, start_val);
+    //printf("This is %d 's end value: %d \n \n", rank, end_val);
+
     /*
-    if (rank == 0) {
-        // print the final solution
-        printf("This is the final solution to the problem: %d ", finalSum);
+    for (int i = start_val; i < end_val; i++) {
+        comp_array[l] = l + 10;
+        l++;
     }
     */
+
+    // calculate fibb sequence positions for the sequence
+
+    int l = 0;
+    long double fibb_const = (1 + sqrt(5)) / 2;  int j = start_val; long double fibb_const1 = (1 - sqrt(5)) / 2;
+    long double position11 = (pow(fibb_const, j) - pow(fibb_const1, j)) / (sqrt(5));
+    long double position12 = (pow(fibb_const, j + 1) - pow(fibb_const1, (j + 1))) / (sqrt(5));
+    position11 = floor(position11);
+    position12 = floor(position12);
+    int pos11 = position11;
+    int pos12 = position12;
+
+    for (int i = 0; i < arraySize; i++) {
+        comp_array[i] = 0;
+    }
+
+
+    // dynamically assign values to each process to generate a solution quickly
+
+    if (rank == 0) {
+        for (int i = start_val; i < end_val + 1; i++) {
+            SumNum = num1 + num2;
+
+            if (num1 % 2 == 0) {
+                comp_array[l] = num1;
+                //printf("Rank 0 Number %d \n \n", num1);
+                l++;
+            }
+
+            num1 = num2;
+            num2 = SumNum;
+        }
+    }
+
+    else {
+        for (int i = start_val; i < end_val + 1; i++) {
+            SumNum = pos12 + pos11;
+
+            if (pos11 % 2 == 0) {
+                comp_array[l] = pos11;
+                //printf("Rank %d Number %d \n \n", rank, pos11);
+                l++;
+            }
+
+            pos11 = pos12;
+            pos12 = SumNum;
+        }
+    }
+
+
+    ////////////define a unqiue array on each process///////////////////////
+
+    for (int i = 0; i < upperLimit; i++) {
+        window_buffer[i] = 0;
+    }
+
+    MPI_Win win;
+    MPI_Win_create(&window_buffer, upperLimit * sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+
+    MPI_Win_fence(0, win);
+
+    MPI_Accumulate(&comp_array[0], arraySize, MPI_INT, 0, 0, arraySize, MPI_INT, MPI_SUM, win);
+        for (int i = 0; i < arraySize; i++) {
+        printf("\n %d \n", comp_array[i]);
+    }
+    MPI_Win_fence(0, win);
+
+    for (int i = 0; i < upperLimit; i++) {
+        finalSum = finalSum + window_buffer[i];
+        printf("I added %d \n", window_buffer[i]);
+    }
+
+    if (rank == 0) {
+        printf("This is my window number %d", finalSum);
+    }
+
     MPI_Win_free(&win);
     MPI_Finalize();
     return 0;
