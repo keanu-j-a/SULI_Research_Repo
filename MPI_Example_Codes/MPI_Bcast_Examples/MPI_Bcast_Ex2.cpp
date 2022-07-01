98252677dc9b6d9690fe716022d78ced620d8fb8
keanu-j-a <107134356+keanu-j-a@users.noreply.github.com>
2022-07-01 02:18:25 -0600

    Update MPI_Bcast_Ex2.cpp

--- MPI_Example_Codes/MPI_Bcast_Examples/MPI_Bcast_Ex2.cpp
+++ MPI_Example_Codes/MPI_Bcast_Examples/MPI_Bcast_Ex2.cpp
@@ -1,36 +1,81 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <mpi.h>
-#include <math.h>
-#include <assert.h>
 
-int main(int argc, char** argv) {
+int inner_det_solver(int x) {
+	
+	int inner_det = 0;
+	inner_det =  x * 10;
 
-    MPI_Init(NULL, NULL);
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    int Max_Size = 5;
-
-    // Create a random array of elements on all processes.
-    int* x = new int[Max_Size];
-    for (int i = 0; i < Max_Size; i++) {
-        x[i] = i + 10;
-    }
-
-    int* global_sum = new int[Max_Size];
-    /*
-    for (int i = 0; i < Max_Size; i++) {
-        global_sum[i] = i + 5;
-    }
-    */
-
-    // Reduce all of the local sums into the global sum in order to
-    MPI_Allreduce(x, global_sum, 2, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    printf("The final sum to process %d is %d", world_rank, global_sum[0]);
-
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Finalize();
+	return inner_det;
 }
+
+int determinate(int x, int y, int  size, int rank) {
+	
+
+	int upperLimit = x; int ans = 0; int j = 0;
+
+	// define the arrays for each variabe
+	int* inner_detArray = (int*)calloc(upperLimit, sizeof(int));
+	int* signArray = (int*)calloc(upperLimit, sizeof(int));
+	int* basicArray = (int*)calloc(upperLimit, sizeof(int));
+
+	// values to update
+	int inner_det = 10, basic = 31, sign = -1;
+	
+	if (rank == 0) {
+		for (j = 0; j < upperLimit; j++) {
+
+			basic = basic + 1;
+
+			*(inner_detArray + y) = inner_det_solver(x);
+			*(basicArray + y) = basic;
+			*(signArray + y) = sign;
+			sign = sign * -1;
+			y++;
+		}
+	}
+
+	/* MPI_Bcast must be called by all processes in order to complete */
+	if (j = upperLimit - 1) {
+		MPI_Bcast(inner_detArray, upperLimit, MPI_INT, 0, MPI_COMM_WORLD);
+		MPI_Bcast(basicArray, upperLimit, MPI_INT, 0, MPI_COMM_WORLD);
+		MPI_Bcast(signArray, upperLimit, MPI_INT, 0, MPI_COMM_WORLD);
+	}
+
+	// other processes do this
+	for (int i = 0; i < upperLimit; i++) {
+
+		// print solutions
+		/*
+		printf("I am rank %d and I have the number: %d \n", rank, inner_detArray[0]);
+		printf("I am rank %d and I have the number: %d \n", rank, basicArray[0]);
+		printf("I am rank %d and I have the number: %d \n", rank, signArray[0]);
+		*/
+		// generate a cumulative solution
+		ans = inner_detArray[i] * basicArray[i] * signArray[i] + ans;
+	}
+
+	return ans;
+
+}
+
+int main(int argc, char* argv[]) {
+
+
+	MPI_Init(&argc, &argv);
+	int rank, size, recieve_buffer = 0;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+		int x = 5;
+		int y = 0;
+		int ans = determinate(x, y, size, rank);
+		printf("\n -------------The main function reports: %d------------- \n", ans);
+
+	MPI_Finalize();
+	return 0;
+
+}
+
